b17c4f4cfc2496f3a47c07a00ae9c83e85c2eec0
patriciapossarii <patricia.mag.carvalho@gmail.com>
2023-03-11 17:30:25 -0300

    created routers

---
+++ src/GlobalStyle.styled.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  #root {
+    
+    margin: 0 auto;
+    background-color:black;
+  }
+`;
---
+++ src/pages/SignupPage/SignupPage.js
@@ -0,0 +1,8 @@
+export default function SignupPage() {
+    return (
+      <>
+        <h1>Página SignupPage</h1>
+      </>
+    );
+  }
+  
